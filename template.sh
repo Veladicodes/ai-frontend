dce0ee63dcc8087c14c6dad7591b38db509b68f6
Sam-uel-Codes <samsuboff@gmail.com>
2025-09-19 21:55:08 +0530

    Folder structure created

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directories
+mkdir -p src
+mkdir -p research 
+
+#Creating files in it. 
+touch app.py
+touch requirement.txt
+touch .env
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch setup.py
+touch research/trials.ipynb
+
+echo "Directories Created"
