b6d205e5d05ee3da2fee4d20de2dab0cd7f336ee
Sam-uel-Codes <samsuboff@gmail.com>
2025-09-19 22:24:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Medi_Chat',
+    author= 'Samuel Subramanian',
+    author_email= 'samsuboff@gmail.com',
+    install_requires= [],
+    version = '0.1.0',
+    packages= find_packages()
+)
